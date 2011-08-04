257d4ccbc29222ee163bd4a273b313e7579a200a
George Vasilakos <forfolias@gmail.com>
2011-08-04 15:09:56 +0300

    edit main screen

--- boof/src/com/vasilakos/boof/Boof.java
+++ boof/src/com/vasilakos/boof/Boof.java
@@ -1,8 +1,11 @@
 package com.vasilakos.boof;
 
 import android.app.Activity;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.Display;
+import android.view.Window;
+import android.widget.TextView;
 
 public class Boof extends Activity {
 
@@ -19,15 +22,23 @@ public class Boof extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+
 
 		setSize();
 
-		Panel p = new Panel(this);
+//		Panel p = new Panel(this);
 
-		setContentView(p);
+//		setContentView(p);
+		setContentView(R.layout.main);
+		
+		TextView txt = (TextView) findViewById(R.id.instructionsTextView);  
+		Typeface font = Typeface.createFromAsset(getAssets(), "fonts/mghobofun.ttf");  
+		txt.setTypeface(font); 
 
-		counter = new MyCount(10000, 100, p);
-		counter.start();
+//		counter = new MyCount(10000, 100, p);
+//		counter.start();
 	}
 	
 	public void setSize(){
