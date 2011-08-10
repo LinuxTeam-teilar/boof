48648de5e5dea43d8e37cf850f5f755e9daffa7e
George Vasilakos <forfolias@gmail.com>
2011-08-10 13:09:37 +0300

    add sound effects

--- boof/src/com/vasilakos/boof/Boof.java
+++ boof/src/com/vasilakos/boof/Boof.java
@@ -7,6 +7,8 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Color;
 import android.graphics.Typeface;
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnCompletionListener;
 import android.os.Bundle;
 import android.view.Display;
 import android.view.View;
@@ -20,14 +22,10 @@ import android.widget.TextView;
 public class Boof extends Activity {
 
 	TextView noOfPlayersEt;
-	
-	Integer[] songs = {
-		R.drawable.song0,
-		R.drawable.song1,
-		R.drawable.song2,
-		R.drawable.song3,
-		R.drawable.song4
-	};
+	MediaPlayer bgMusic;
+
+	Integer[] songs = { R.drawable.song0, R.drawable.song1, R.drawable.song2,
+			R.drawable.song3, R.drawable.song4 };
 
 	// MyCount counter;
 	public static Integer size;
@@ -35,8 +33,9 @@ public class Boof extends Activity {
 	@Override
 	public void onStop() {
 		super.onStop();
-
+		bgMusic.release();
 		// counter.cancel();
+
 	}
 
 	@Override
@@ -44,10 +43,8 @@ public class Boof extends Activity {
 		super.onCreate(savedInstanceState);
 
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
-		
-		setContentView(R.layout.main);
 
-		
+		setContentView(R.layout.main);
 
 		setSize();
 
@@ -61,10 +58,20 @@ public class Boof extends Activity {
 				"fonts/ubscript.ttf");
 		instrTv.setTypeface(font);
 		noOfPlayersEt.setTypeface(font);
-		
-		Gallery ga = (Gallery)findViewById(R.id.song_select_gallery);
+
+		Gallery ga = (Gallery) findViewById(R.id.song_select_gallery);
 		ga.setAdapter(new ImageAdapter(this));
-		
+
+		bgMusic = MediaPlayer.create(this, R.raw.childrenplaying);
+		bgMusic.setLooping(true);
+		bgMusic.start();
+
+		// try {
+		// mp = MediaPlayer.create(this, R.raw.splat1);
+		// } catch (Exception e) {
+		// Log.d("KOKO", e.getLocalizedMessage());
+		// }
+
 		// counter = new MyCount(10000, 100, p);
 		// counter.start();
 	}
@@ -84,9 +91,9 @@ public class Boof extends Activity {
 	}
 
 	public void plusButtonClicked(View v) {
-		playPliatsSound();
 		Integer a = Integer.parseInt(noOfPlayersEt.getText().toString());
 		if (a < 9) {
+			playSplashSound();
 			a++;
 			noOfPlayersEt.setText(a.toString());
 			setRandomTextColor();
@@ -96,70 +103,87 @@ public class Boof extends Activity {
 	public void minusButtonClicked(View v) {
 		Integer a = Integer.parseInt(noOfPlayersEt.getText().toString());
 		if (a > 2) {
+			playSplashSound();
 			a--;
 			noOfPlayersEt.setText(a.toString());
 			setRandomTextColor();
 		}
 	}
-	
-	public void setRandomTextColor(){
+
+	public void setRandomTextColor() {
 		Random random = new Random();
-		int randomColor = Color.rgb(random.nextInt(255), random.nextInt(255), random.nextInt(255));
+		int randomColor = Color.rgb(random.nextInt(255), random.nextInt(255),
+				random.nextInt(255));
 		noOfPlayersEt.setTextColor(randomColor);
 	}
 
-	public void playPliatsSound() {
+	public void playSplashSound() {
 
-		// new Thread() {
-		// public void run() {
-		// MediaPlayer mp = MediaPlayer.create(Boof.this, R.raw.splat);
-		//
-		// mp.setOnCompletionListener(new OnCompletionListener() {
-		//
-		// public void onCompletion(MediaPlayer mp) {
-		// mp.release();
-		// }
-		// });
-		// mp.start();
-		// }
-		// }.start();
+		new Thread() {
+			public void run() {
+				MediaPlayer mp = MediaPlayer.create(Boof.this, R.raw.splash);
+
+				mp.setOnCompletionListener(new OnCompletionListener() {
+
+					public void onCompletion(MediaPlayer mp) {
+						mp.release();
+					}
+				});
+				mp.start();
+			}
+		}.start();
 	}
-	
-    public class ImageAdapter extends BaseAdapter {
 
-    	private Context ctx;
-    	
-    	public ImageAdapter(Context c) {
+	public void boofButtonClicked(View v) {
+		new Thread() {
+			public void run() {
+				MediaPlayer mp = MediaPlayer.create(Boof.this, R.raw.splash);
+				mp.setOnCompletionListener(new OnCompletionListener() {
+					public void onCompletion(MediaPlayer mp) {
+						mp.release();
+					}
+				});
+				mp.start();
+			}
+		}.start();
+
+	}
+
+	public class ImageAdapter extends BaseAdapter {
+
+		private Context ctx;
+
+		public ImageAdapter(Context c) {
 			ctx = c;
 			TypedArray ta = obtainStyledAttributes(R.styleable.Gallery1);
 			ta.recycle();
 		}
 
 		@Override
-    	public int getCount() {
-    		
-    		return songs.length;
-    	}
+		public int getCount() {
 
-    	@Override
-    	public Object getItem(int arg0) {
-    		
-    		return arg0;
-    	}
+			return songs.length;
+		}
 
-    	@Override
-    	public long getItemId(int arg0) {
-    		
-    		return arg0;
-    	}
+		@Override
+		public Object getItem(int arg0) {
 
-    	@Override
-    	public View getView(int arg0, View arg1, ViewGroup arg2) {
-    		ImageView iv = new ImageView(ctx);
-    		iv.setImageResource(songs[arg0]);
-    		iv.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
-//    		iv.setLayoutParams(new Gallery.LayoutParams(144,50));
-    		return iv;
-    	}
-    }
+			return arg0;
+		}
+
+		@Override
+		public long getItemId(int arg0) {
+
+			return arg0;
+		}
+
+		@Override
+		public View getView(int arg0, View arg1, ViewGroup arg2) {
+			ImageView iv = new ImageView(ctx);
+			iv.setImageResource(songs[arg0]);
+			iv.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+			// iv.setLayoutParams(new Gallery.LayoutParams(144,50));
+			return iv;
+		}
+	}
 }
